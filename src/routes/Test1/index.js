e488b4773a2f7db37cfb6d7d63f7c9916eefe037
Gayuh Ridho <gayuh.ridho@tokopedia.com>
2022-08-01 14:35:53 +0700

    Test1 done

--- src/routes/Test1/index.js
+++ src/routes/Test1/index.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 const Test1 = () => {
+  const [text, setText] = useState('');
+
   return(
     <div>
       <ul>
         <li>Please render the inputted value</li>
       </ul>
       <p>
-        <b>User Input: </b> ______
+        <b>User Input: </b> {text}
       </p>
-      <input type="text" placeholder="input here"/>
+      <input type="text" placeholder="input here" onChange={(e) => setText(e.target.value)}/>
     </div>
   )
 }
